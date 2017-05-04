82269a83c56d570c84a4c0a658326a5ae90fc487
Miguel Isasmendi <miguel.isasmendi@globant.com>
2017-05-03 23:18:38 -0300

    fixing import

--- src/models/init.js
+++ src/models/init.js
@@ -1,11 +1,11 @@
-const uuid = require('node-uuid')
+const uuid = require('uuid')
 const moment = require ('moment')
 const path = require('path')
 const logger = require('log4js').getLogger('Models Initializator')
 
 function createModels ( connection, mongoose, normalizedPath ) {
   logger.debug(`Creating Models...`)
-  
+
   let mongoBuildArguments = {}
   require(path.join(normalizedPath, 'CellsGrid'))(connection, mongoose, mongoBuildArguments)
   logger.debug(`Models created so far: ${JSON.stringify(mongoBuildArguments)}`)
